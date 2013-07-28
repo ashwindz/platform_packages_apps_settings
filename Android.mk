89321fbeb36291a74c012b7fe8a2590939973661
Martin Rodemann <martin@rodemann.org>
2013-07-28 15:33:33 +0200

    Revert "Switch to koush superuser"
    This reverts commit 7ad9637c034ef8d8b8febc8de43367db6ec0f85a.

--- Android.mk
+++ Android.mk
@@ -13,9 +13,7 @@ LOCAL_CERTIFICATE := platform
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-LOCAL_AAPT_FLAGS := --extra-packages com.koushikdutta.superuser:com.koushikdutta.widgets -S $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res -S $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res --auto-add-overlay
-LOCAL_SRC_FILES += $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
+LOCAL_AAPT_FLAGS += -c zz_ZZ
 
 include $(BUILD_PACKAGE)
 
